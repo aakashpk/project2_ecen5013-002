234102c869d7882076ccd5aece56e5285dd94237
Miles Frain <miles.frain@colorado.edu>
2018-04-29 14:10:28 -0600

    modularized dirfile handling, fixed flushing

--- demo/custom_dirfile.c
+++ demo/custom_dirfile.c
@@ -34,17 +34,13 @@ can setup a flush interval for performance customization
 
 #include "packet_data_type.h"
 
-void write_flush(FILE *fp, void *data, size_t len, uint32_t flush_period)
+void write_flush(FILE *fp, void *data, size_t len, bool force_flush)
 {
-    static uint32_t call_count = 0;
-    call_count++;
-
     assert(1 == fwrite(data, len, 1, fp));
 
-    if (call_count >= flush_period)
+    if (force_flush)
     {
         assert(0 == fflush(fp));
-        call_count = 0;
     }
 }
 
@@ -360,29 +356,118 @@ void *datagen_task(void *ptr)
     return NULL;
 }
 
+typedef struct
+{
+    FILE *fp_speed;
+    FILE *fp_setpoint;
+    FILE *fp_error;
+
+    FILE *fp_pwm_output;
+
+    FILE *fp_p_value;
+    FILE *fp_i_value;
+    FILE *fp_d_value;
+
+    FILE *fp_kp;
+    FILE *fp_ki;
+    FILE *fp_kd;
+
+    FILE *fp_auto_tune;
+    FILE *fp_update_period_ns;
+    FILE *fp_windup_limit;
+} dir_handles_t;
+
+void create_dirfile(dir_handles_t *handle, char* dir)
+{
+    handle->fp_speed = fopen_dir_check(dir, "speed", "w");
+    handle->fp_setpoint = fopen_dir_check(dir, "setpoint", "w");
+    handle->fp_error = fopen_dir_check(dir, "error", "w");
+
+    handle->fp_pwm_output = fopen_dir_check(dir, "pwm_output", "w");
+
+    handle->fp_p_value = fopen_dir_check(dir, "p_value", "w");
+    handle->fp_i_value = fopen_dir_check(dir, "i_value", "w");
+    handle->fp_d_value = fopen_dir_check(dir, "d_value", "w");
+
+    handle->fp_kp = fopen_dir_check(dir, "kp", "w");
+    handle->fp_ki = fopen_dir_check(dir, "ki", "w");
+    handle->fp_kd = fopen_dir_check(dir, "kd", "w");
+
+    handle->fp_auto_tune = fopen_dir_check(dir, "auto_tune", "w");
+    handle->fp_update_period_ns = fopen_dir_check(dir, "update_period_ns", "w");
+    handle->fp_windup_limit = fopen_dir_check(dir, "windup_limit", "w");
+}
+
+void close_dirfile(dir_handles_t *handle)
+{
+    fclose(handle->fp_speed);
+    fclose(handle->fp_setpoint);
+    fclose(handle->fp_error);
+
+    fclose(handle->fp_pwm_output);
+
+    fclose(handle->fp_p_value);
+    fclose(handle->fp_i_value);
+    fclose(handle->fp_d_value);
+
+    fclose(handle->fp_kp);
+    fclose(handle->fp_ki);
+    fclose(handle->fp_kd);
+
+    fclose(handle->fp_auto_tune);
+    fclose(handle->fp_update_period_ns);
+    fclose(handle->fp_windup_limit);
+}
+
+void write_dirfile_entry(dir_handles_t *handle,
+                         motor_values_t *motor_values,
+                         pid_param_t *pid_param,
+                         pid_config_t *pid_config,
+                         uint32_t flush_interval)
+{
+    // Check if we need to force a flush
+    // Longer interval could potentially enable better performance if file writes are slow
+    static uint32_t unflushed_write_count = 0;
+    unflushed_write_count++;
+    bool force_flush = false;
+    if (unflushed_write_count >= flush_interval)
+    {
+        force_flush = true;
+        unflushed_write_count = 0;
+        printf("flushing\n");
+    }
+
+    // -------------- Write all structs to files -----------------
+    // could use sizeof for each field instead of magic number 4, but that gets too busy
+
+    write_flush(handle->fp_speed, (void *)&motor_values->speed, 4, force_flush);
+    write_flush(handle->fp_setpoint, (void *)&motor_values->setpoint, 4, force_flush);
+    write_flush(handle->fp_error, (void *)&motor_values->error, 4, force_flush);
+
+    write_flush(handle->fp_pwm_output, (void *)&motor_values->pwm_output, 4, force_flush);
+
+    write_flush(handle->fp_p_value, (void *)&motor_values->p_value, 4, force_flush);
+    write_flush(handle->fp_i_value, (void *)&motor_values->i_value, 4, force_flush);
+    write_flush(handle->fp_d_value, (void *)&motor_values->d_value, 4, force_flush);
+
+    write_flush(handle->fp_kp, (void *)&pid_param->kp, 4, force_flush);
+    write_flush(handle->fp_ki, (void *)&pid_param->ki, 4, force_flush);
+    write_flush(handle->fp_kd, (void *)&pid_param->kd, 4, force_flush);
+
+    write_flush(handle->fp_auto_tune, (void *)&pid_config->auto_tune, 4, force_flush);
+    write_flush(handle->fp_update_period_ns, (void *)&pid_config->update_period_ns, 4, force_flush);
+    write_flush(handle->fp_windup_limit, (void *)&pid_config->windup_limit, 4, force_flush);
+}
+
 int main(void)
 {
     printf("starting\n");
+
     // --------------- open files --------------
     char dir[] = "demo_dir";
 
-    FILE *fp_speed = fopen_dir_check(dir, "speed", "w");
-    FILE *fp_setpoint = fopen_dir_check(dir, "setpoint", "w");
-    FILE *fp_error = fopen_dir_check(dir, "error", "w");
-
-    FILE *fp_pwm_output = fopen_dir_check(dir, "pwm_output", "w");
-
-    FILE *fp_p_value = fopen_dir_check(dir, "p_value", "w");
-    FILE *fp_i_value = fopen_dir_check(dir, "i_value", "w");
-    FILE *fp_d_value = fopen_dir_check(dir, "d_value", "w");
-
-    FILE *fp_kp = fopen_dir_check(dir, "kp", "w");
-    FILE *fp_ki = fopen_dir_check(dir, "ki", "w");
-    FILE *fp_kd = fopen_dir_check(dir, "kd", "w");
-
-    FILE *fp_auto_tune = fopen_dir_check(dir, "auto_tune", "w");
-    FILE *fp_update_period_ns = fopen_dir_check(dir, "update_period_ns", "w");
-    FILE *fp_windup_limit = fopen_dir_check(dir, "windup_limit", "w");
+    dir_handles_t dir_handles;
+    create_dirfile(&dir_handles, dir);
 
     // --------------------------------------------
 
@@ -408,7 +493,7 @@ int main(void)
     // File read will return 0 bytes at EOF and trigger loop exit
     // This is fine for testing. Other communication schemes will support better blocking.
 
-    sleep(1); // allow file to be created
+    sleep(1); // allow file to be created while debugging
 
     // single shot debug
     //pthread_join(datagen_thread, NULL);
@@ -590,42 +675,14 @@ int main(void)
 
                     // Todo - how to deal with synchronizing data across files?
                     // Simple solution is to just do all writes in here.
-                    // Messy, but this must happen here. Could also move below with setting flag.
+                    // This is one of the non-ideal aspects of dirfile timeseries, which wouldn't be an issue in custom plotter
                     // Must write within packet processing loop to handle multipe incoming motor packets per read cycle.
 
-                    // copy data in structs other than motor_values(fastest updating) to local copy,
-                    // since these won't update as frequently, but still need to logged in timeseries.
-                    // This is one of the non-ideal aspects of dirfile timeseries, which wouldn't be an issue in custom plotter
+                    // Last parameter is flush interval
+                    write_dirfile_entry(&dir_handles, &motor_values, &pid_param, &pid_config, 10);
 
-                    // This is broken for values other than 1, since increments for every call of shared function.
-                    // Needs to be connected to be related to each file handle, although not great for less frequent writes.
-                    //         -- note for above, all writes are same rate
-                    // Better is to use a flag connected to timer.
-                    // Should also just double-check if flushing improves plotting results (can be done with flag).
-                    uint32_t flush_period = 1; // flush every write, but can slow this down for better performance
-
-                    // -------------- Write all structs to files -----------------
-                    // could use sizeof for each field instead of magic number 4, but that gets too busy
-
-                    write_flush(fp_speed, (void *)&motor_values.speed, 4, flush_period);
-                    write_flush(fp_setpoint, (void *)&motor_values.setpoint, 4, flush_period);
-                    write_flush(fp_error, (void *)&motor_values.error, 4, flush_period);
-
-                    write_flush(fp_pwm_output, (void *)&motor_values.pwm_output, 4, flush_period);
-
-                    write_flush(fp_p_value, (void *)&motor_values.p_value, 4, flush_period);
-                    write_flush(fp_i_value, (void *)&motor_values.i_value, 4, flush_period);
-                    write_flush(fp_d_value, (void *)&motor_values.d_value, 4, flush_period);
-
-                    write_flush(fp_kp, (void *)&pid_param.kp, 4, flush_period);
-                    write_flush(fp_ki, (void *)&pid_param.ki, 4, flush_period);
-                    write_flush(fp_kd, (void *)&pid_param.kd, 4, flush_period);
-
-                    write_flush(fp_auto_tune, (void *)&pid_config.auto_tune, 4, flush_period);
-                    write_flush(fp_update_period_ns, (void *)&pid_config.update_period_ns, 4, flush_period);
-                    write_flush(fp_windup_limit, (void *)&pid_config.windup_limit, 4, flush_period);
-
-                    // --------------------------------------------
+                    // To simulate continuous file read while debugging
+                    usleep(1E4);
 
                     break;
                 }
@@ -644,52 +701,11 @@ int main(void)
 
     printf("read zero bytes (EOF) - main loop exiting\n");
 
+    close_dirfile(&dir_handles);
+
 #if GEN_ENABLE
     pthread_join(datagen_thread, NULL);
 #endif
 
     return 0;
 }
-
-/*
-# measured from tachometer
-speed RAW FLOAT32 1
-# desired speed in RPS
-setpoint RAW FLOAT32 1
-# measured - desired
-error RAW FLOAT32 1
-
-# sent to motor
-pwm_output RAW UINT32 1
-
-# calculated terms
-p_value RAW FLOAT32 1
-i_value RAW FLOAT32 1
-d_value RAW FLOAT32 1
-
-# pid parameters
-# these updates are not sent as frequently,
-# so latest must be maintained by c-struct to dirfile converter
-# to be re-written to dirfile every frame
-kp RAW FLOAT32 1
-ki RAW FLOAT32 1
-kd RAW FLOAT32 1
-
-
-# Below fields do not update often
-
-# auto tune enabled
-# bool, but can allow faster writes (in exchange for disk space),
-# by writing 32-bit word.
-# writing bits is also allowed, but may not be worth slowdown,
-since flushing every write. Would be more ideal if flushing after many samples.
-auto_tune RAW UINT32 1
-
-# period of PID loop (time between updates),
-# not the same as time interval between logged samples.
-# Units of ns, which can handle period of up to 4 seconds
-update_period_ns RAW UINT32 1
-
-# maximum absolute limit of integration term
-windup_limit RAW FLOAT32 1
-*/
