3fb43708735819bbfff8856eacecde2b4059c873
Miles Frain <miles.frain@colorado.edu>
2018-04-29 21:06:30 -0600

    re-arranged bbb files for p2

--- bbb/sources.mk
+++ bbb/sources.mk
@@ -1,27 +1,27 @@
-MAIN_SOURCE=src/main.c
+#MAIN_SOURCE=src/main.c
+MAIN_SOURCE=src/main_tester.c
 
-COMMON_SOURCES=src/socketserver.c \
-			src/logger.c \
-			src/common_queue.c \
-			src/bidirectional_queue.c \
-			src/single_ended_queue.c\
-			src/tasks.c
+COMMON_SOURCES= \
+			src/dirfile_writer.c \
+			../common/src/data_gen.c \
+			../common/src/data_output.c \
+			../common/src/file_helper.c \
+			../common/src/packet_parser.c \
+			../common/src/packet_writer.c \
+			../common/src/packet_data_type.c
+#			src/socketserver.c \
+#			src/packet_comms.c \
 
-BBB_SOURCES=src/tempsensor.c\
-	src/my_i2c.c\
-	src/lightsensor.c\
-	src/sensordata.c
+#BBB_SOURCES=src/tempsensor.c\
 
-HOST_SOURCES=src/dummydata.c
+#HOST_SOURCES=src/dummydata.c
 
-WRAPPED_TEST_SOURCES:=test/sensor_test.c
+#WRAPPED_TEST_SOURCES:=test/sensor_test.c
 
-TEST_SOURCES=test/simple_test.c \
+#TEST_SOURCES=test/simple_test.c \
 		   test/queue_test.c
 
-CLIENT_SOURCES=src/socketclient.c
+#CLIENT_SOURCES=src/socketclient.c
 
 MOCKED_FUNCTIONS=
 #	test/bd_queue_test.c
-
-
