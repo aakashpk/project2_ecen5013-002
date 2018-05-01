8d5f0afb31dc1e7a5cbe870d0f6558acd3c00d63
Miles Frain <miles.frain@colorado.edu>
2018-05-01 08:34:32 -0600

    options on launch

--- bbb/src/main_tester.c
+++ bbb/src/main_tester.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #include "data_gen.h"
 #include "bbb_packet_handling.h"
@@ -19,41 +20,63 @@ int main(int argc, char **argv)
 {
     printf("starting\n");
 
-    char* input_path = "combo_structs";
-    char* output_dirfile_path = "demo_dir";
+    char* input_path = "structs_stream";
+    char* output_dirfile_path = "dirfile";
 
-    // Override input path
-    if (argc > 1)
-    {
-        input_path = argv[1];
-        printf("input path changed to %s\n", input_path);
-    }
+    int gen_flag = 0;
+    int block_flag = 0;
+    int c;
+
+    while ((c = getopt(argc, argv, "hf:gb")) != -1)
+        switch (c)
+        {
+        case 'h':
+            printf("\n"
+                   "-f file"
+                   "-g gen flag"
+                   "-b block flag"
+                   "-h help\n");
+            break;
+        case 'f':
+            input_path = optarg;
+            break;
+        case 'g':
+            gen_flag = 1;
+            break;
+        case 'b':
+            block_flag = 1;
+            break;
+        default:
+            abort();
+        }
+
+
+    printf("input path %s\n", input_path);
 
     int retval; // pthread_create return value
 
-#define GEN_ENABLE 0
-#define GEN_BLOCK 0
-#if GEN_ENABLE
     // datagen for testing
     pthread_t datagen_thread;
-
-    if ((retval = pthread_create(&datagen_thread, NULL, datagen_task, (void *)input_path)))
+    if (gen_flag)
     {
-        printf("pthread create issue %s\n", strerror(retval));
-        abort();
+        if ((retval = pthread_create(&datagen_thread, NULL, datagen_task, (void *)input_path)))
+        {
+            printf("pthread create issue %s\n", strerror(retval));
+            abort();
+        }
+
+        // File read will return 0 bytes at EOF and trigger loop exit
+        // This is fine for testing. Other communication schemes will support better blocking.
+
+        sleep(1); // allow file to be created while debugging
+
+        if (block_flag)
+        {
+            // Force continuous or time-limited data generation
+            pthread_join(datagen_thread, NULL);
+        }
     }
 
-    // File read will return 0 bytes at EOF and trigger loop exit
-    // This is fine for testing. Other communication schemes will support better blocking.
-
-    sleep(1); // allow file to be created while debugging
-
-#if GEN_BLOCK
-    // Force continuous or time-limited data generation
-    pthread_join(datagen_thread, NULL);
-#endif
-
-#endif // GEN_ENABLE
 
     // Todo - call packet parser function/task
     pthread_t bbb_packet_parsing_thread;
@@ -72,9 +95,10 @@ int main(int argc, char **argv)
     // Todo - task to setup ncurses UI in this or separate task, and write commands back to source of structs stream
     // Printf logs need to be replaced with log to file. Will need to pass this path into tasks.
 
-#if GEN_ENABLE
-    pthread_join(datagen_thread, NULL);
-#endif
+    if (gen_flag)
+    {
+        pthread_join(datagen_thread, NULL);
+    }
 
     pthread_join(bbb_packet_parsing_thread, NULL);
     printf("exiting\n");
